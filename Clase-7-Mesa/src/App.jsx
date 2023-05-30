bb30d9f2c757098fc7390d706dfd565969c20b34
crxstian <cristian19overdrive@gmail.com>
2023-05-29 22:23:51 -0300

    Cargando Template Parcial Front

---
+++ Clase-7-Mesa/src/App.jsx
@@ -0,0 +1,16 @@
+import react from 'react'
+import './App.css'
+import Componente1 from './Components/Componente1'
+
+function App() {
+  
+
+  return (
+    <>
+    <h3>Clase 7 Actividad Mesa 6</h3>
+    <Componente1 />
+    </>
+  )
+}
+
+export default App
---
+++ Clase-7-Mesa/src/Components/Componente1.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Componente1 = () => {
+  return (
+    <>
+    <ul>
+        <button onClick={()=>}>Punto 1</button>
+        <li>Punto 1</li>
+        <li>Punto 2</li>
+        <li>Punto 3</li>
+    </ul>
+    </>
+  )
+}
+
+export default Componente1
