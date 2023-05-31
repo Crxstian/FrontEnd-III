533c5383213a73aa638b7a34075f63b59aff2246
Cristian Siles <100167590+Crxstian@users.noreply.github.com>
2023-05-31 14:30:05 -0300

    Update Card.jsx

--- Clase 10/src/Components/Card.jsx
+++ Clase 10/src/Components/Card.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({cumple, styles}) => {
+  const estilos = {backgroundColor: styles.background, color: styles.color}
   return (
-    <div className='card'>
-      <h3>Hola! , Estas Invitado al cumple de {}</h3>
-      <h4>Te espero el dia: {} a las {} hs</h4>
+    <div className='card' style={estilos}>
+      <h3>Hola! , Estas Invitado al cumple de {cumple.nombre}</h3>
+      <h4>Te espero el dia: {cumple.fecha} a las 18 hs</h4>
       <h2>No faltes!!</h2>
-      <h4>Invitacion Para: {}</h4>
+      <h4>Invitacion Para: {cumple.invitado}</h4>
     </div>
   )
 }
