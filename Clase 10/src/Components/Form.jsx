1c25422f160e56c13fd3d608b1fad91d5402bfe2
Cristian Siles <100167590+Crxstian@users.noreply.github.com>
2023-05-31 14:30:46 -0300

    Update Form.jsx

--- Clase 10/src/Components/Form.jsx
+++ Clase 10/src/Components/Form.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Form = ({setCumple}) => {
+const Form = ({setCumple, setStyles }) => {
   return (
     <div>
         <label>Nombre del que cumple:</label>
@@ -11,6 +11,31 @@ const Form = ({setCumple}) => {
 
         <label>Nombre del invitado</label>
         <input type="text" onChange={(e)=> setCumple((estadoAnterior)=>({...estadoAnterior, invitado: e.target.value}))}/>
+        
+        
+        <label>Fondo:</label>
+        <select onChange={(e)=>setStyles((prevStyles)=>({...prevStyles, background: e.target.value}))}>
+          <option value="transparent">transparent</option>
+          <option value="black">black</option>
+          <option value="yellow">yellow</option>
+          <option value="red">red</option>
+          <option value="pink">pink</option>
+          <option value="orange">orange</option>
+          <option value="blue">blue</option>
+          <option value="green">green</option>
+        </select>
+        <label>color de letra:</label>
+        <select onChange={(e)=>setStyles((prevStyles)=>({...prevStyles, color: e.target.value}))}>
+          <option value="white">white</option>
+          <option value="black">black</option>
+          <option value="yellow">yellow</option>
+          <option value="red">red</option>
+          <option value="pink">pink</option>
+          <option value="orange">orange</option>
+          <option value="blue">blue</option>
+          <option value="green">green</option>
+        </select>
+
     </div>
   )
 }
